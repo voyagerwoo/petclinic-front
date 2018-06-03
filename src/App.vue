3c5d2b736419126937a1fd482cd1004275875743
voyager.woo <voyager.woo@gmail.com>
2018-06-04 01:07:51 +0900

    Add staff page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <h2><a href="">AK 동물병원</a></h2>
+    <h2><a href="#/">AK 동물병원</a></h2>
     <ul>
       <li><a href="#/staff">직원 소개</a></li>
-      <li><a href="">예약</a></li>
-      <li><a href="">나의 펫</a></li>
-      <li><a href="">펫 사진</a></li>
+      <li><a href="#/">예약</a></li>
+      <li><a href="#/">나의 펫</a></li>
+      <li><a href="#/">펫 사진</a></li>
     </ul>
     <router-view/>
   </div>
--- src/pages/Staff.vue
+++ src/pages/Staff.vue
@@ -2,6 +2,15 @@
   <div>
     <h1>AK 직원 소개</h1>
 
+    <div v-for="vet in vets">
+      <h2>{{vet.lastName}}{{vet.firstName}}</h2>
+      {{vet.description}}
+      <div class="special-tag">
+        <code>"{{vet.year}}년차"</code>
+        <code v-for="specialty in vet.specialties">"{{specialty.name}}"</code>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -9,8 +18,13 @@
 import restService from '../services/restService'
 export default {
   name: 'Staffs',
+  data: function() {
+    return {
+      vets : []
+    }
+  },
   created () {
-    restService.fetchVets().then(r => console.log(r))
+    restService.fetchVets().then(r => {console.log(r); this.vets = r})
   }
 }
 </script>
