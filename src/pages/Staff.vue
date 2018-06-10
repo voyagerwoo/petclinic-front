500971348113a513daaacb54be80ef8683f3ce4f
voyager.woo <voyager.woo@gmail.com>
2018-06-11 00:59:55 +0900

    Fix lint errors

--- src/pages/Staff.vue
+++ src/pages/Staff.vue
@@ -2,12 +2,12 @@
   <div>
     <h1>AK 직원 소개</h1>
 
-    <div v-for="vet in vets">
+    <div v-for="vet in vets" v-bind:key="vet.id">
       <h2>{{vet.lastName}}{{vet.firstName}}</h2>
       {{vet.description}}
       <div class="special-tag">
         <code>"{{vet.year}}년차"</code>
-        <code v-for="specialty in vet.specialties">"{{specialty.name}}"</code>
+        <code v-for="specialty in vet.specialties" v-bind:key="specialty.id">"{{specialty.name}}"</code>
       </div>
     </div>
 
@@ -18,13 +18,13 @@
 import restService from '../services/restService'
 export default {
   name: 'Staffs',
-  data: function() {
+  data: function () {
     return {
-      vets : []
+      vets: []
     }
   },
   created () {
-    restService.fetchVets().then(r => {console.log(r); this.vets = r})
+    restService.fetchVets().then(r => { console.log(r); this.vets = r })
   }
 }
 </script>
