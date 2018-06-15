e46a6906ea8ca7a187f230343c0dab7ae35557bb
voyager.woo <voyager.woo@gmail.com>
2018-06-16 01:38:04 +0900

    Add components

---
+++ src/pages/Booking.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="booking">
+    <markdown-contents markdown-name="underConstruction"></markdown-contents>
+  </div>
+</template>
+
+<script>
+import MarkdownContents from '../components/MarkdownContents'
+
+export default {
+  components: {
+    MarkdownContents
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.booking img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ static/markdowns/underConstruction.md
@@ -0,0 +1,3 @@
+# 작업중입니다!
+
+![](/static/images/construction.jpg)
