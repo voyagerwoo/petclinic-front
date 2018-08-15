2431d04e2b02b2cfead887368f8870490ec36d3d
voyagerwoo <voyager.woo@gmail.com>
2018-08-16 00:17:01 +0900

    Add bulma

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'bulma/css/bulma.min.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 /* eslint-disable no-new */
 new Vue({
