9f56c29ea6b186ce2b33a3fd0057481228f15dda
voyagerwoo <voyager.woo@gmail.com>
2018-08-16 02:30:52 +0900

    Update staff introduction pages

--- src/pages/Staff.vue
+++ src/pages/Staff.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
-    <h1>AK 직원 소개</h1>
-
-    <div v-for="vet in vets" v-bind:key="vet.id">
-      <h2>{{vet.lastName}}{{vet.firstName}}</h2>
-      {{vet.description}}
-      <div class="special-tag">
-        <code>"{{vet.year}}년차"</code>
-        <code v-for="specialty in vet.specialties" v-bind:key="specialty.id">"{{specialty.name}}"</code>
+    <div class="hero">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">AK 동물병원 직원소개</h1>
+        </div>
       </div>
     </div>
 
+    <div class="row columns is-tablet is-multiline">
+      <div class="column is-one-third" v-for="vet in vets" v-bind:key="vet.id+'card'">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="/static/images/no-imagesdf.jpg" alt="Placeholder image" onerror="this.src='/static/images/no-image.jpg'">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">{{vet.lastName}}{{vet.firstName}}</p>
+                <!--TODO <p class="subtitle is-6">@johnsmith</p>-->
+              </div>
+            </div>
+
+            <div class="content">
+              {{vet.description}}
+              <p>
+                <code>#{{vet.year}}년차</code><code v-for="specialty in vet.specialties" v-bind:key="specialty.id">#{{specialty.name}}</code>
+              </p>
+              <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,5 +54,7 @@ export default {
 </script>
 
 <style scoped>
-
+code {
+  margin-left: 5px;
+}
 </style>
